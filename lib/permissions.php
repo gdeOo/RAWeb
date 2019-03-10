b4dbae7b30f34eb494985093022a5672d8022b09
luchaos <luchaos@gmail.com>
2019-03-10 11:58:46 +0100

    add back superuser as jr. dev to  PermissionsToString()

--- lib/permissions.php
+++ lib/permissions.php
@@ -1,6 +1,6 @@
 <?php
 function PermissionsToString( $permissions )
 {
-	$permissionsStr = [ "Spam", "Banned", "Unregistered", "Registered", "Developer", "Moderator", "Admin", "Root" ];
+	$permissionsStr = [ "Spam", "Banned", "Unregistered", "Registered", "Jr. Developer", "Developer", "Moderator", "Admin", "Root" ];
 	return $permissionsStr[$permissions - ( \RA\Permissions::Spam )]; //	Offset of 0
 }
