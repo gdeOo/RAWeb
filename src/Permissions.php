31324dc94083869c554215031c5e8c991eedcb78
luchaos <luchaos@gmail.com>
2019-03-10 11:38:31 +0100

    add back superuser as jrdev

--- src/Permissions.php
+++ src/Permissions.php
@@ -8,8 +8,9 @@ abstract class Permissions
 	const Banned = -1;
 	const Unregistered = 0;
 	const Verified = 1;
-	const Developer = 2;
-	const Moderator = 3;
-	const Admin = 4;
-	const Root = 5;
+	const JrDeveloper = 2;
+	const Developer = 3;
+	const Moderator = 4;
+	const Admin = 5;
+	const Root = 6;
 }
